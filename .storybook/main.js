358f971e5c77c0dcc06d43e8e6620434d41de22e
Salih Yılmaz <salih_yil@live.com>
2022-12-11 16:48:21 +0300

    MDX and build storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,10 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-docs'
+    '@storybook/preset-create-react-app'
   ],
   framework: '@storybook/react',
   core: {
